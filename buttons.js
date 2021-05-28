21d3ad1b56f5e4f71616bcede5d7a9ce4e0b5ab2
jonquiiillll <71707079+jonquiiillll@users.noreply.github.com>
2021-05-28 18:19:18 +0300

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,151 @@
+$(document).ready(function(){
+	$('.content_toggle').click(function(){
+		$('.content_block').slideToggle(300);
+		return false;
+	});
+});
+	$('.box_black').click(function(){
+		$('.content_block').slideToggle(300);
+		return false;
+	});
+  $('.content_toggle').on("click", function() {
+    $(".box_black").toggleClass('ordered');
+    $(".content_toggle").toggleClass('underline');
+});
+  $('.box_black').on("click", function() {
+    $(".box_black").toggleClass('ordered');
+      $(".content_toggle").toggleClass('underline');
+});
+$('.order_window').on("click", function() {
+  $(".order_window").addClass('ordered');
+});
+$('.order_window').click(function(){
+  	$(this).html('ЗАБРОНИРОВАННО');
+});
+
+
+$(document).ready(function(){
+	$('.content_toggle_2').click(function(){
+		$('.content_block_2').slideToggle(300);
+		return false;
+	});
+});
+	$('.box_black_2').click(function(){
+		$('.content_block_2').slideToggle(300);
+		return false;
+	});
+  $('.content_toggle_2').on("click", function() {
+    $(".box_black_2").toggleClass('ordered');
+    $(".content_toggle_2").toggleClass('underline');
+});
+  $('.box_black_2').on("click", function() {
+    $(".box_black_2").toggleClass('ordered');
+    $(".content_toggle_2").toggleClass('underline');
+});
+$('.order_window_2').on("click", function() {
+  $(".order_window_2").addClass('ordered');
+});
+$('.order_window_2').click(function(){
+  	$(this).html('ЗАБРОНИРОВАННО');
+});
+
+
+$(document).ready(function(){
+	$('.content_toggle_3').click(function(){
+		$('.content_block_3').slideToggle(300);
+		return false;
+	});
+});
+	$('.box_black_3').click(function(){
+		$('.content_block_3').slideToggle(300);
+		return false;
+	});
+  $('.content_toggle_3').on("click", function() {
+    $(".box_black_3").toggleClass('ordered');
+    $(".content_toggle_3").toggleClass('underline');
+});
+  $('.box_black_3').on("click", function() {
+    $(".box_black_3").toggleClass('ordered');
+    $(".content_toggle_3").toggleClass('underline');
+});
+$('.order_window_3').on("click", function() {
+  $(".order_window_3").addClass('ordered');
+});
+$('.order_window_3').click(function(){
+  	$(this).html('ЗАБРОНИРОВАННО');
+});
+
+$(document).ready(function(){
+	$('.content_toggle_4').click(function(){
+		$('.content_block_4').slideToggle(300);
+		return false;
+	});
+});
+	$('.box_black_4').click(function(){
+		$('.content_block_4').slideToggle(300);
+		return false;
+	});
+  $('.content_toggle_4').on("click", function() {
+    $(".box_black_4").toggleClass('ordered');
+    $(".content_toggle_4").toggleClass('underline');
+});
+  $('.box_black_4').on("click", function() {
+    $(".box_black_4").toggleClass('ordered');
+    $(".content_toggle_4").toggleClass('underline');
+});
+$('.order_window_4').on("click", function() {
+  $(".order_window_4").addClass('ordered');
+});
+$('.order_window_4').click(function(){
+  	$(this).html('ЗАБРОНИРОВАННО');
+});
+
+$(document).ready(function(){
+	$('.content_toggle_5').click(function(){
+		$('.content_block_5').slideToggle(300);
+		return false;
+	});
+});
+	$('.box_black_5').click(function(){
+		$('.content_block_5').slideToggle(300);
+		return false;
+	});
+  $('.content_toggle_5').on("click", function() {
+    $(".box_black_5").toggleClass('ordered');
+    $(".content_toggle_5").toggleClass('underline');
+});
+  $('.box_black_5').on("click", function() {
+    $(".box_black_5").toggleClass('ordered');
+    $(".content_toggle_5").toggleClass('underline');
+});
+$('.order_window_5').on("click", function() {
+  $(".order_window_5").addClass('ordered');
+});
+$('.order_window_5').click(function(){
+  	$(this).html('ЗАБРОНИРОВАННО');
+});
+
+$(document).ready(function(){
+	$('.content_toggle_6').click(function(){
+		$('.content_block_6').slideToggle(300);
+		return false;
+	});
+});
+	$('.box_black_6').click(function(){
+		$('.content_block_6').slideToggle(300);
+		return false;
+	});
+  $('.content_toggle_6').on("click", function() {
+    $(".box_black_6").toggleClass('ordered');
+    $(".content_toggle_6").toggleClass('underline');
+});
+  $('.box_black_6').on("click", function() {
+    $(".box_black_6").toggleClass('ordered');
+    $(".content_toggle_6").toggleClass('underline');
+});
+$('.order_window_6').on("click", function() {
+  $(".order_window_6").addClass('ordered');
+});
+$('.order_window_6').click(function(){
+  	$(this).html('ЗАБРОНИРОВАННО');
+});
